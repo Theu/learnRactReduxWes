49b74fed23303e87ab3413d9030a9032f135605e
Theu <info@matteocontini.com>
2017-01-17 12:14:12 +0100

    created the ROUTER- maybe finally understood

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// css
 import css from './styles/style.styl';
 
+// components
 import Main from './components/Main';
+import Photogrid from './components/Photogrid';
+import Single from './components/Single';
 
-render(<Main/>, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Photogrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
