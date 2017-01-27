55652c0718ed617a0af38fef1d14d1ff89aefe4f
Matteo Contini <matteo.contini@zalando-lounge.de>
2017-01-27 20:07:46 +0100

    13 added comment and created comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 import { Link } from 'react-router';
 
 export default class Single extends React.Component {
   render() {
+    // index of the post
+    //   get the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    console.log(post);
     return (
       <div className="single-photo">
-      I am the single
+          <Photo i={i} post={post} {...this.props} />
+          <Comments />
       </div>
     );
   }
